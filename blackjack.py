18d601657407d4476fabca95d6668fc3a710ea14
Rachel Eisenbrandt <racheleisenbrandt@gmail.com>
2021-06-14 16:51:05 -0400

    added welcome messge & add delay

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,11 @@
-from hashlib import new
-from typing import Coroutine
+import time
 from hand import Hand
 from deck import Deck
 from card import Card
+
+# sleep delay intervals
+delay = 1
+delay2 = 2
 
 def score_hand(hand):
     value = 0
@@ -23,7 +26,7 @@ def score_hand(hand):
 def results(player_hand, dealer_hand):
     if score_hand(dealer_hand) > 17:
         if score_hand(dealer_hand) > 21:
-            print('The dealer busts. You win.')
+            print('The dealer busts. You win!')
         elif score_hand(player_hand) > score_hand(dealer_hand):
             print('Congratulations! You win!')
         elif score_hand(player_hand) < score_hand(dealer_hand):
@@ -32,33 +35,45 @@ def results(player_hand, dealer_hand):
             print('It\'s a tie!')
     return ""
 
-play = input('Do you want to play? ').lower()
+print("""
+Welcome to The Casino.
+Where the bets don't matter, and the money isn't real.
+*Gambling not encouraged*
+""")
+
+play = input('Would you like to play? ').lower()
 while play[0] == 'y':
-    # print('right')
     new_deck = Deck()
     new_deck.create_deck()
     new_deck.shuffle()
 
-    # print('here')
     player_hand = Hand()
     dealer_hand = Hand()
 
     print('\nYour first card is a ')
     player_hand.add_card(new_deck.draw_one())
+    time.sleep(delay)
     player_hand.print_status()
 
-    print('\nThe dealers first card is a ')
+    time.sleep(delay2)
+    print('\nThe dealer\'s first card is a ')
     dealer_hand.add_card(new_deck.draw_one())
+    time.sleep(delay)
     dealer_hand.print_status()
 
+    time.sleep(delay2)
     print('\nYour second card is a ')
     player_hand.add_card(new_deck.draw_one())
+    time.sleep(delay)
     player_hand.print_status()
 
+    time.sleep(delay2)
+    print('\nThe dealer is dealt a card face down.')
     dealer_hand.add_card(new_deck.draw_one())
 
+    time.sleep(delay)
     print(f'\nYour total score is: {score_hand(player_hand)}')
-
+    time.sleep(delay)
     player_bust = False
 # player turn hit or stand phase
     if score_hand(player_hand) < 21:
@@ -66,11 +81,14 @@ while play[0] == 'y':
         while hit_stand[0] != 's':
             new_card = new_deck.draw_one()
             player_hand.add_card(new_card)
+            time.sleep(delay)
+            print('\nYour new card is a ')
             new_card.print_status()
-            # player_hand.print_status()
 
-            print(score_hand(player_hand))
+            time.sleep(delay)
+            print(f'For a total of: {score_hand(player_hand)}')
             if score_hand(player_hand) > 21:
+                time.sleep(delay)
                 print('You bust. Better luck next time.')
                 hit_stand = 's'
                 player_bust = True
@@ -78,55 +96,31 @@ while play[0] == 'y':
                 print('Blackjack!')
                 hit_stand = 's'
             else:
+                time.sleep(delay)
                 hit_stand = input('Do you want to hit or stand? ')
-
-            # print(score_hand(player_hand))
 
 # dealer turn hit or stand phase
     if player_bust == False:
+        time.sleep(delay)
         print('\nThe dealer flips over his face down card. Showing a ')
+        time.sleep(delay)
         dealer_hand.print_status()
-
-        print('\nFor a total of: ')
-        print(score_hand(dealer_hand))
+        print(f'\nFor a total of: {score_hand(dealer_hand)}')
 
         while score_hand(dealer_hand) < 17:
             dealer_card = new_deck.draw_one()
-            print('The dealer hits and receives a ')
+            print('\nThe dealer hits and receives a ')
             dealer_hand.add_card(dealer_card)
+            time.sleep(delay)
             dealer_card.print_status()
-            print('\nFor a total of ')
-            print(score_hand(dealer_hand))
+            print(f'\nFor a total of: {score_hand(dealer_hand)}')
 
 # comparison of scores to determine a winner
     print(results(player_hand, dealer_hand))
 
+    time.sleep(delay2)
     play = input('Would you like to play again? ')
 
 
-
-
-        # while score_hand(dealer_hand) > 17:
-        #     if score_hand(player_hand) > score_hand(dealer_hand):
-        #         print('Congratulations! You win!')
-        #     elif score_hand(player_hand) < score_hand(dealer_hand):
-        #         print('Unlucky... you lose. Better luck next time.')
-        #     elif score_hand(player_hand) == score_hand(dealer_hand):
-        #         print('It\'s a tie!')
-
-        # if hand_compare == 'done':
-        #     if score_hand(player_hand) > score_hand(dealer_hand):
-        #         print('Congratulations! You win!')
-        #     elif score_hand(player_hand) < score_hand(dealer_hand):
-        #         print('Unlucky... the dealer wins!')
-        #     elif score_hand(player_hand) == score_hand(dealer_hand):
-        #         print('It\'s a tie!')
-
-            
-
-
-        # else:
-        #     hit_stand = input('Do you want to hit or stand? ')
-            
 
 
