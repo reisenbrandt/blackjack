68c7ee42adc196623922a90dd74bf33a25e45fad
Rachel Eisenbrandt <racheleisenbrandt@gmail.com>
2021-06-15 19:18:18 -0400

    betting

--- blackjack.py
+++ blackjack.py
@@ -11,6 +11,7 @@ delay2 = 2
 # determines the values of the card and adds to a total
 def score_hand(hand):
     value = 0
+    evaluate_ace = 0
     # for each card in the hand
     for card in hand.cards:
         # face cards are always 10
@@ -18,46 +19,60 @@ def score_hand(hand):
             value += 10
         # aces can be 1 or 11 based on the total - automatically determines the best use of ace for the user
         elif card.value == 'A':
-            if value + 11 <= 21:
-                value += 11
-            else:
-                value += 1
+            evaluate_ace += 1
         # otherwise, cards have face value
         else:
             value += int(card.value)
 
+    for x in range(evaluate_ace):
+        if value + 11 <= 21:
+            value += 11
+        else:
+            value += 1
+
     return value
 
+
+start = 500
 # determines the winner of the game
-def results(player_hand, dealer_hand):
+def results(player_hand, dealer_hand, start):
     # as long as the dealer has a hand of 17 or more
     if score_hand(dealer_hand) >= 17:
         # if dealer over 21 - dealer bust
         if score_hand(dealer_hand) > 21:
             print('The dealer busts. You win!')
+            start += bet
+            print(f'You now have ${start}!')
         # player beats dealer
         elif score_hand(player_hand) > score_hand(dealer_hand):
             print('Congratulations! You win!')
+            start += bet
+            print(f'You now have ${start}!')
         # dealer beats player
         elif score_hand(player_hand) < score_hand(dealer_hand):
             print('Unlucky... you lose. Better luck next time.')
-        # totals are the same - automatic tie
+            start -= bet
+            print(f'You now have ${start}.')
+        # totals are the same -automatic tie
         elif score_hand(player_hand) == score_hand(dealer_hand):
             print('It\'s a tie!')
-    return ""
+            print(f'You don\'t gain or lose any money. You have ${start}.')
+
+    return start
 
 
 print("""
 Welcome to The Casino.
+You get $500 on the house!
 *Gambling not encouraged*
 """)
 # Where the bets aren't real, and the money doesn't matter.
 # ask if user wants to play to start the loop
 play = input('Would you like to play? ').lower()
 
-# bet = int(input('How much would you like to bet? '))
 # as long as the player wants to play, loop runs
 while play[0] == 'y':
+    bet = int(input('How much would you like to bet? '))
     # instantiate a deck, creates cards, and shuffles
     new_deck = Deck()
     new_deck.create_deck()
@@ -94,7 +109,7 @@ while play[0] == 'y':
     time.sleep(delay1)
     player_bust = False
     # player turn hit or stand phase - if player has not bust
-    if score_hand(player_hand) < 21:
+    if score_hand(player_hand) <= 21:
         # ask the user what to do, as long as not stand, loop runs
         hit_stand = input('Do you want to hit or stand? ')
         while hit_stand[0] != 's':
@@ -111,6 +126,8 @@ while play[0] == 'y':
             if score_hand(player_hand) > 21:
                 time.sleep(delay1)
                 print('You bust. Better luck next time.')
+                start -= bet
+                print(f'You now have ${start}.')
                 hit_stand = 's'
                 player_bust = True
             # if player gets 21, its a natural blackjack but still need to see if dealer gets 21
@@ -144,11 +161,14 @@ while play[0] == 'y':
             time.sleep(delay1)
 
         # comparison of scores to determine a winner
-        print(results(player_hand, dealer_hand))
+        start = results(player_hand, dealer_hand, start)
+        # print(start)
+
 
     time.sleep(delay2)
-    play = input('Would you like to play again? ')
-
+    play = input('\nWould you like to play again? ')
+else:
+    print('\nSee you next time!')
 
 
 
